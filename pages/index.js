a5e53f5b13746085251c3c0913479ee9af31ee65
olcaneristi <ogulcaneristi123@gmail.com>
2020-07-03 08:58:50 +0300

    layout eklendi

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,10 @@
-import About from '../components/about'
-import Contact from '../components/contact'
-import Resume from '../components/resume'
-import Header from '../components/header'
-import Footer from '../components/footer'
-import Home from '../components/home'
-import Head from 'next/head';
+import Layout from '../components/layout'
+import * as React from 'react'
 
-export default () => (
-  <div className="app">
-    <Head>
-    <link rel="shortcut-icon" href="/public/favicon.ico"/>
-        <title>Oğulcan Erişti </title>
+function Index(){
+  return(
+    <Layout/>
+  )
+}
 
-        <meta name="title1" content="Oğulcan Erişti " />
-
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-
-        <meta
-          name="keyword"
-          content="reactjs, react, javascript, js, nextjs, next, ogulcan, eristi, oğulcan erişti, oğulcan, erişti, junior, jr, frontend, developer, frontend developer"
-        />
-
-        <meta
-          name="description"
-          content="I'm Oğulcan Erişti. I'm Jr. Frontend developer and MIS student."
-        />
-    </Head>
-
-    
-    <Header/>
-    <Home/>
-    <About/>
-    <Resume/>
-    <Contact/>
-    <Footer/>
-  </div>
-)
+export default Index;
