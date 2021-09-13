2014f3a8daded7fafa3ce761952c34978473ea88
ogulcan eristi <ogulcaneristi123@gmail.com>
2021-09-14 00:11:15 +0300

    add dummy 404 page

--- src/components/404/index.js
+++ src/components/404/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../../styles/pages/404.module.scss';
 
 const NotFound = () => {
-  return <div>404 Not Found</div>;
+  return (
+    <div className={styles.notfound}>
+      <h1 className={styles.title}>404 Not Found!</h1>
+      <p>You can </p>
+      <Link to="/">go back</Link>
+    </div>
+  );
 };
 
 export default NotFound;
