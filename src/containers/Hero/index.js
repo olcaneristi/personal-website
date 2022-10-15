770b9bb7ec542bdbbe3c2b5890ba46f92ef16bb7
olcaneristi <ogulcaneristi123@gmail.com>
2022-10-15 15:21:19 +0300

    update description and portfolio url

--- src/containers/Hero/index.js
+++ src/containers/Hero/index.js
@@ -3,7 +3,7 @@ import styles from '../../styles/pages/hero.module.scss';
 import { ScrollToTop } from '../../hooks/';
 
 const Hero = () => {
-  const resumeURL = 'https://drive.google.com/file/d/1F-GIW4mqPPytxg0l7JR8tvX75AgVTjeq/view';
+  const resumeURL = 'https://drive.google.com/file/d/13gmJybdMzfQe7hcYjmjoj0NFHtVkB6gY/view';
   const profilePhoto = 'https://avatars.githubusercontent.com/u/45832621?v=4';
 
   return (
@@ -16,8 +16,9 @@ const Hero = () => {
           <h2>Ogulcan Eristi.</h2>
           <h3>I build beautiful things for the web!</h3>
           <p>
-            I'm a freshly graduated <span>Frontend Developer</span> who builds interfaces for users to have great
-            experiences, trying to improve himself on the web and enjoys it.
+            I'm a <span>Frontend Developer</span> building pretty UI's for users to have great experiences, 
+            trying to improve myself on the web & mobile and enjoys it. I'm known for my calmness but I get 
+            very excited about new ideas.
           </p>
           <button className={`${styles.btn} ${styles.btn__cta}`}>
             <a href={resumeURL} target="_blank" rel="noopener noreferrer">
