b98dedd1ea0ba2878de5adfabb411b32563ceea2
ogulcan eristi <ogulcaneristi123@gmail.com>
2024-07-09 16:17:49 +0300

    update hero and resume

--- src/containers/Hero/index.js
+++ src/containers/Hero/index.js
@@ -3,7 +3,7 @@ import styles from '../../styles/pages/hero.module.scss';
 import { ScrollToTop } from '../../hooks/';
 
 const Hero = () => {
-  const resumeURL = 'https://drive.google.com/file/d/13gmJybdMzfQe7hcYjmjoj0NFHtVkB6gY/view';
+  const resumeURL = 'https://drive.google.com/file/d/1OrI4n_oXar1Xy55CPyFGrlusHnDyIBP5/view';
   const profilePhoto = 'https://avatars.githubusercontent.com/u/45832621?v=4';
 
   return (
@@ -16,9 +16,9 @@ const Hero = () => {
           <h2>Ogulcan Eristi.</h2>
           <h3>I build beautiful things for the web!</h3>
           <p>
-            I'm a <span>Frontend Developer</span> who builds pretty UI's for users to have great experiences, 
-            trying to improve myself on the web & mobile and enjoys it. I'm known for my calmness but I get 
-            very excited about new ideas.
+            I'm a <span>Frontend Developer</span> with 3 years of relevant work experience in building visually appealing and user-friendly interfaces for
+            web and mobile applications. Passionate about continuous learning and self-improvement, I’m dedicated to enhancing user
+            experiences. I’m enthusiastic about exploring new ideas and technologies.
           </p>
           <button className={`${styles.btn} ${styles.btn__cta}`}>
             <a href={resumeURL} target="_blank" rel="noopener noreferrer">
